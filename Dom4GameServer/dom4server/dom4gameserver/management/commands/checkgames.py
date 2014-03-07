0c768e33857125a3f11db19d4a1b7b8122a9150c
TenjouUtena <utena@proactiveapathy.com>
2014-03-07 22:36:55 +0000

    =* checkgames command ver 0.1 finished

--- Dom4GameServer/dom4server/dom4gameserver/management/commands/checkgames.py
+++ Dom4GameServer/dom4server/dom4gameserver/management/commands/checkgames.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
-from dom3gameserver.models import Game
-from subprocess import check_output
+from dom4gameserver.models import Game
+from subprocess import check_output, call
+from optparse import make_option
 import re
 
 
@@ -15,22 +16,23 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        if options['gamename'] == 'all':
+        if options['gamename'] != 'all':
             self.stdout.write('Unimplemented Feature.')
         else:
             processes = check_output(['ps','ax']).splitlines()
             prore = ".*?dom4.*?%s"
 
 
-        for gg in Game.objects.filter(status__in = ['PRETENDER','RUNNING']):
-            found = False
-            for pro in processes:
-                mo = re.search(prore % gg.servername,pro)
-                if mo:
-                    found = True
-                    self.stdout.write("Game: %s found in line: %s" % (gg.name, pro))
-            if not found:
-                self.stdout.write("I didn't find game: %s" % gg.name)
+            for gg in Game.objects.filter(status__in = ['PRETENDER','RUNNING']):
+                found = False
+                for pro in processes:
+                    mo = re.search(prore % gg.servername,pro)
+                    if mo:
+                        found = True
+                        self.stdout.write("Game: %s found in line: %s" % (gg.name, pro))
+                if not found:
+                    self.stdout.write("I didn't find game: %s.   Attempting to Restart..." % gg.name)
+                    call("./%s" % gg.servername,cwd="/var/dom4/games/")
 
 
 
