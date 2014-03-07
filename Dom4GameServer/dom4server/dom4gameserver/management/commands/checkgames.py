3b7790af79f853ca8efc6bce6789cff0479b8cb4
TenjouUtena <utena@proactiveapathy.com>
2014-03-07 15:12:01 -0600

    * Fixed whitespace in files.

--- Dom4GameServer/dom4server/dom4gameserver/management/commands/checkgames.py
+++ Dom4GameServer/dom4server/dom4gameserver/management/commands/checkgames.py
@@ -15,23 +15,23 @@ class Command(BaseCommand):
         )
 
 
-	def handle(self, *args, **options):
-		if options['gamename'] == 'all':
-			self.stdout.write('Unimplemented Feature.')
-		else:
-			processes = check_output(['ps','ax']).splitlines()
-			prore = ".*?dom4.*?%s"
+        def handle(self, *args, **options):
+        if options['gamename'] == 'all':
+            self.stdout.write('Unimplemented Feature.')
+        else:
+            processes = check_output(['ps','ax']).splitlines()
+            prore = ".*?dom4.*?%s"
 
 
-			for gg in Game.objects.filter(status__in = ['PRETENDER','RUNNING']):
-				found = False
-				for pro in processes:
-					mo = re.search(prore % gg.servername,pro)
-					if mo:
-						found = True
-						self.stdout.write("Game: %s found in line: %s" % (gg.name, pro))
-				if not found:
-					self.stdout.write("I didn't find game: %s" % gg.name)
+        for gg in Game.objects.filter(status__in = ['PRETENDER','RUNNING']):
+            found = False
+            for pro in processes:
+                mo = re.search(prore % gg.servername,pro)
+                if mo:
+                    found = True
+                    self.stdout.write("Game: %s found in line: %s" % (gg.name, pro))
+            if not found:
+                self.stdout.write("I didn't find game: %s" % gg.name)
 
 
 
--- Dom4GameServer/dom4server/dom4gameserver/models.py
+++ Dom4GameServer/dom4server/dom4gameserver/models.py
@@ -2,40 +2,40 @@ from django.db import models
 
 
 class Game(models.Model):
-	GAME_STATUSES = [
-		('NEW','New Game'),
-		('PRETENDER','Game Accepting Pretenders'),
-		('RUNNING','Game Running'),
-		('STOPPED','Game Stopped'),
-		('COMPLETE','Game Complete')
-	]
+    GAME_STATUSES = [
+        ('NEW','New Game'),
+        ('PRETENDER','Game Accepting Pretenders'),
+        ('RUNNING','Game Running'),
+        ('STOPPED','Game Stopped'),
+        ('COMPLETE','Game Complete')
+    ]
 
-	name = models.CharField(max_length=500)
-	servername = models.CharField(max_length=64)
-	status = models.CharField(max_length=15, choices=GAME_STATUSES)
+    name = models.CharField(max_length=500)
+    servername = models.CharField(max_length=64)
+    status = models.CharField(max_length=15, choices=GAME_STATUSES)
 
-	def __unicode__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
 
 class ServerCommand(models.Model):
-	SERVER_COMMANDS = [
-		('RESET','Reset Timer'),
-		('STOP','Stop Game'),
-		('START','Start (or Restart) Game'),
-		('SETTIMER','Set a Games Timer')
-	]
-	COMMAND_STATUS = [
-		('NEW','New Command'),
-		('EXECUTING','Command being processed'),
-		('EXECUTED','Command complete')
-	]
-	command = models.CharField(max_length=15, choices=SERVER_COMMANDS)
-	arg1 = models.CharField(max_length=15)
-	status = models.CharField(max_length=15, choices=COMMAND_STATUS)
-	game = models.ForeignKey(Game)
+    SERVER_COMMANDS = [
+        ('RESET','Reset Timer'),
+        ('STOP','Stop Game'),
+        ('START','Start (or Restart) Game'),
+        ('SETTIMER','Set a Games Timer')
+    ]
+    COMMAND_STATUS = [
+        ('NEW','New Command'),
+        ('EXECUTING','Command being processed'),
+        ('EXECUTED','Command complete')
+    ]
+    command = models.CharField(max_length=15, choices=SERVER_COMMANDS)
+    arg1 = models.CharField(max_length=15)
+    status = models.CharField(max_length=15, choices=COMMAND_STATUS)
+    game = models.ForeignKey(Game)
 
-	def __unicode__(self):
-		return self.game.name + " " + self.command + " " + self.arg1
+    def __unicode__(self):
+        return self.game.name + " " + self.command + " " + self.arg1
 
-	
+
 
