9bcfa46ed05d85324569e02efbbb4da723da9bd0
TenjouUtena <utena@proactiveapathy.com>
2014-03-07 13:14:04 -0600

    * More Admin Cleanup
    Signed-off-by: TenjouUtena <utena@proactiveapathy.com>

--- Dom4GameServer/dom4server/dom4gameserver/admin.py
+++ Dom4GameServer/dom4server/dom4gameserver/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from dom4gameserver.models import Game, ServerCommand
+ 
+
+class GameAdmin(admin.PollAdmin):
+	list_display = ('name','status')
+
 
 admin.site.register(Game)
 admin.site.register(ServerCommand)
